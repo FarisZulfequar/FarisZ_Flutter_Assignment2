ba3fcbfef453904ff0c6882268feaf8e004e0718
Faris Zulfequar <zulfequf@shernet.sheridancollege.ca>
2026-03-13 11:10:29 -0400

    Added the rest of the methods for the Statistics Page

--- NumberContainer.tsx
+++ NumberContainer.tsx
@@ -1,5 +1,4 @@
 import React, {createContext, ReactNode, useState} from "react";
-import { Text } from 'react-native';
 
 interface NumberContainerType {
   numberMap: Record<number, number>;
@@ -18,6 +17,33 @@ export const NumberContainer = createContext<NumberContainerType>({
 });
 
 export const NumberContainerProvider = ({children}: ProviderProps) => {
- return (<Text></Text>)
+  const [numberMap, setNumberMap] = useState<Record<number, number>>({
+    1: 0, 2: 0, 3: 0,
+    4: 0, 5: 0, 6: 0,
+    7: 0, 8: 0, 9: 0,
+  });
+
+  const resetNumberList = () => {
+    setNumberMap({
+      1: 0, 2: 0, 3: 0,
+      4: 0, 5: 0, 6: 0,
+      7: 0, 8: 0, 9: 0,
+    });
+  };
+
+  const increaseGeneratedNumberTimes = (num : number) => {
+    setNumberMap(prev => ({
+      ...prev,
+      [num]: (prev[num] || 0) + 1
+    }));
+  };
+
+  return (
+    <NumberContainer.Provider
+      value={{ numberMap, resetNumberList, increaseGeneratedNumberTimes }}
+    >
+      {children}
+    </NumberContainer.Provider>
+  );
 
 }
